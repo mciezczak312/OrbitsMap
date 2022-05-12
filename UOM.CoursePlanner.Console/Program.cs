7fc4657c4d5ebd27566e4c24a92bf86ad778f14d
Mateusz Ciezczak <mciezczak312@gmail.com>
2022-05-12 20:29:49 +0300

    add: console app

---
+++ UOM.CoursePlanner.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿using UOM.CoursePlanner.Validator;
+
+Console.WriteLine("==== Orbits Map Validator ====");
+Console.Write("map file name: ");
+var mapFileName = Console.ReadLine();
+
+var mapParser = new MapParser();
+var universeMap = await mapParser.ParseMapFileAsync(mapFileName);
+var checkSum = mapParser.CalculateMapChecksum(universeMap);
+Console.WriteLine($"Total number of direct and indirect orbit: {checkSum}");
